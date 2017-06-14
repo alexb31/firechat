fe8b3b17696acbbdaa1ff800fc32b2e935b43ded
alexb31 <123azertyu>
2017-06-14 02:56:12 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth'
+import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  name: any;
+  items: FirebaseListObservable<any>;
+  msgVal: string = '';
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 5
+      }
+    });
+
+    this.name = this.afAuth.authState;
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  }
+
+  chatSend(theirMessage: string) {
+    this.items.push({ message: theirMessage, name: this.name.faceboo.displayName });
+    this.msgVal = '';
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCZkQIsn1SGOtk81cbtWgPFAqNts36PXyg",
+  authDomain: "mychatapp-a867e.firebaseapp.com",
+  databaseURL: "https://mychatapp-a867e.firebaseio.com",
+  projectId: "mychatapp-a867e",
+  storageBucket: "mychatapp-a867e.appspot.com",
+  messagingSenderId: "328747334597"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
